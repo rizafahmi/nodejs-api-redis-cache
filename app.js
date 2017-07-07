5c7c8404aee9b79ec342c221a0404d1299632680
Riza Fahmi <rizafahmi@gmail.com>
2017-07-07 14:48:19 +0700

    Finish the app

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
-const responseTime = require('responseTime')
-const axios = require('axios')
+const responseTime = require('response-time')
 const redis = require('redis')
+const github = require('./github')
 
 const client = redis.createClient()
 
@@ -12,7 +12,41 @@ client.on('error', err => {
 app.set('port', process.env.PORT || 3000)
 app.use(responseTime())
 
-app.get('/', (req, res) => {})
+app.get('/', (req, res) => {
+  res.send('welcome to caching experience!')
+})
+app.get('/api/:username', (req, res) => {
+  const username = req.params.username
+
+  client.get(username, (error, result) => {
+    if (result) {
+      res.send({
+        totalStars: result,
+        source: 'redis cache'
+      })
+    } else {
+      github
+        .getUserRepositories(username)
+        .then(github.computeTotalStars)
+        .then(totalStars => {
+          client.setex(username, 60, totalStars)
+          res.send({
+            totalStars: totalStars,
+            source: 'GitHub API'
+          })
+        })
+        .catch(response => {
+          if (response.status === 404) {
+            res.send(
+              'The GitHub username could not be found. Try "rizafahmi" as an example.'
+            )
+          } else {
+            res.send(response)
+          }
+        })
+    }
+  })
+})
 
 app.listen(app.get('port'), () => {
   console.log('Magic happen at http://localhost:', app.get('port'))
--- github.js
+++ github.js
@@ -1,13 +1,20 @@
-export const getUserRepositories = user => {
+const axios = require('axios')
+
+const getUserRepositories = user => {
   const endpoint = `https://api.github.com/users/${user}/repos?per_page=100`
   return axios.get(endpoint)
 }
 
-export const computeTotalStars = repositories => {
+const computeTotalStars = repositories => {
   return repositories.data.reduce(
     (prev, curr) => {
       return prev + curr.stargazers_count
     },
     0
   )
+}
+
+module.exports = {
+  getUserRepositories,
+  computeTotalStars
 }
